56541ad00f65d4313f3c957f69506aed983a2efd
jihun.bang <uwlgnsu@naver.com>
2021-04-06 03:15:54 +0900

    [Life Cycle] 자세한 설명 추가
    [DESC]
    1. onRestart() 함수 추가
    2. 자세한 설명 추가

--- LifeCycle/app/src/main/java/jihun/bang/lifesycle/MainActivity.kt
+++ LifeCycle/app/src/main/java/jihun/bang/lifesycle/MainActivity.kt
@@ -13,42 +13,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // Layout 설정
         setContentView(R.layout.activity_main)
-
         Log.d(TAG, "[MainActivity][onCreate] called")
     }
 
-    private fun onButtonClick() {
-        Log.d(TAG, "[MainActivity][onButtonClick] called")
-    }
-
+    // Activity 실행
     override fun onStart() {
         // super: 기존 onStart 로직 가져오기
         super.onStart()
-
         Log.d(TAG, "[MainActivity][onStart] called")
     }
 
+    // 수행 후 Activity 화면 출력
     override fun onResume() {
         super.onResume()
-
         Log.d(TAG, "[MainActivity][onResume] called")
     }
 
+    // Activity 전환되어 화면에 보이지 않음.
+    // Activity 백그라운드 상태
     override fun onPause() {
         super.onPause()
-
         Log.d(TAG, "[MainActivity][onPause] called")
     }
 
+    // Activity 중단
     override fun onStop() {
         super.onStop()
-
         Log.d(TAG, "[MainActivity][onStop] called")
     }
 
+    // Activity 재실행
+    // 백그라운드의 Activity 화면 출력
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "[MainActivity][onStop] called")
+    }
+
+    // Activity 종료
     override fun onDestroy() {
         super.onDestroy()
-
         Log.d(TAG, "[MainActivity][onDestroy] called")
     }
 }
